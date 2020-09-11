80f67ac8e7801195cece5e93382e0306d347391f
李道欢 <379198689@qq.com>
2020-09-12 01:07:10 +0800

    ...

--- app/extend/context.ts
+++ app/extend/context.ts
@@ -1,18 +1,18 @@
 
 module.exports = {
-    success (data, status=200, msg='成功') {
-        this.status = status;
-        this.body = {
-            code: status,
-            data,
-            msg
-        }
-    },
-    error (status=500, msg:string='失败') {
-        this.status = status;
-        this.body = {
-            code: status,
-            msg
-        }
-    },
+  success(data, status = 200, msg = '成功') {
+    this.status = status;
+    this.body = {
+      code: status,
+      data,
+      msg,
+    };
+  },
+  error(status = 500, msg = '失败') {
+    this.status = status;
+    this.body = {
+      code: status,
+      msg,
+    };
+  },
 };
